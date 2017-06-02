448767ec706a7320d3e2045e64683347b2056fed
danieltbrad <danieltbrad@gmail.com>
2017-06-01 20:30:50 -0700

    Broken shell script implementation for Nate

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM ruby:2.3.3
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
 RUN mkdir /myapp
 WORKDIR /myapp
-ADD Gemfile /myapp/Gemfile
-ADD Gemfile.lock /myapp/Gemfile.lock
+RUN rm /bin/sh && ln -s /bin/bash /bin/sh
+COPY Gemfile* startup ./
 RUN bundle install
-ADD . /myapp
+# ADD . /myapp
+# ENTRYPOINT bash startup.sh && bundle exec rails s -p 3000 -b '0.0.0.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Post.create(title: "First Post", body: "body of first post", author: "Daniel")
+Post.create(title: "Second Post", body: "body of second post", author: "Daniel")
+Post.create(title: "Third Post", body: "body of third post", author: "Louisa")
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
